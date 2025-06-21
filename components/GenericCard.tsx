5e6ca652b9f71aa999c718b7e19e4d5a68bc0e50
Necobgs <emanuelborgesdev@gmail.com>
2025-06-21 00:19:52 -0300

    generic card created

---
+++ components/GenericCard.tsx
@@ -0,0 +1,30 @@
+import Inventory from "@/interfaces/InventoryInterface";
+import { StyleSheet,Text } from "react-native";
+import { RelativePathString, useRouter } from "expo-router";
+import { Card } from "react-native-paper";
+import GenericCardProps from "@/interfaces/GenericCardProps";
+
+
+
+export default function GenericCard(props:GenericCardProps){
+    const router = useRouter()
+    
+    return (
+    <Card style={styles.container} mode="elevated" onPress={()=>router.navigate(props.navigateURL as RelativePathString) }>
+    <Card.Title title={props.title}/>   
+    <Card.Content>
+        <Text numberOfLines={3}>
+            {props.description}
+        </Text>    
+    </Card.Content> 
+    </Card>  
+    )
+}
+
+const styles= StyleSheet.create({
+    container:{
+        marginBottom:15, 
+        height:125,
+        maxWidth:400
+    },
+})
